104e53694920d544f98818d37500f19c1bb1c20e
Guilherme SIlva <gs2281172@gmail.com>
2024-03-27 15:38:02 -0300

    Secret_Drawman

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function sortear()
+{
+    let quantidade = document.getElementById('quantidade').Value;
+    let de = document.getElementById('de').Value;
+    let ate = document.getElementById('ate').Value;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
+}
