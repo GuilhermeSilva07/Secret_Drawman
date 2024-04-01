7c23ccc9020526a653ca4076ddde457fc86fd502
Guilherme SIlva <gs2281172@gmail.com>
2024-04-01 14:53:50 -0300

    Configurando a funcão de gerar numero aleatorio

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 
-function sortear()
-{
-    let quantidade = document.getElementById('quantidade').Value;
-    let de = document.getElementById('de').Value;
-    let ate = document.getElementById('ate').Value;
 
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até o número: ${ate}`);
+// Funcionalidade para puxar os dados que são preencidos atraves do Front-End
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);   // a variavel foi definida como 'parseint', para entrar apenas numeros dentro dela
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++){      //Configurando para que o numero seja gerado quantas vezes foram passadas no front
+        numero = obterNumeroAleatorio(de,ate);
+        sorteados.push(numero);
+    }
+    
+    alert(sorteados);
+}
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
 }
