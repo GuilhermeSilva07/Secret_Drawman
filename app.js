685a28d14b88c9b3705e9d288cbc98a6d567b303
Guilherme SIlva <gs2281172@gmail.com>
2024-04-02 10:31:31 -0300

    Função reiniciar e alterarstatusbotao

--- app.js
+++ app.js
@@ -17,14 +17,44 @@ function sortear(){
             numero = obterNumeroAleatorio(de,ate);
         }
 
-
         sorteados.push(numero);
     }
     
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`  //Configuração para exibir a quantidade de numeros no front
+
+    alterarStatusBotao(); 
 }
 
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//Funcão para ativação  do botão reiniciar
+
+function alterarStatusBotao(){
+
+    let botao = document.getElementById('btn-reiniciar');
+
+    if (botao.classList.contains('container__botao-desabilitado'))
+    {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }
+    else 
+    {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+
+function reiniciar()  // função para configurar o botao reiniciar
+{
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+
+    alterarStatusBotao();
 }
